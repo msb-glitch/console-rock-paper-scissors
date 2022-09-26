3331c5b124a839513102be4e7b4bf8c11656e61b
M. Scott Brauer <msb@mscottbrauer.com>
2022-09-25 22:40:20 -0400

    Create 5 round game and ask to play again at end

--- javascript.js
+++ javascript.js
@@ -9,31 +9,89 @@ function getComputerChoice() {
 
 
 
-const computerSelection = getComputerChoice();
-const playerSelection = prompt("Please choose: Rock, Paper, Scissors");
+
+
 
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
     let result = '';
-    
+
     if (playerSelection === computerSelection) { // Draw conditions: player selection === computer selection
-        result = `P: ${playerSelection} === C: ${computerSelection} -> Draw`;
+        result = 'draw';
     }
-    
+
     else if ((playerSelection === 'rock' && computerSelection === 'paper') ||  // Losing conditions: computer beats paper
         (playerSelection === 'scissors' && computerSelection === 'rock') ||
         (playerSelection === 'paper' && computerSelection === 'scissors')) {
 
-        result = `P: ${playerSelection} <> C: ${computerSelection} -> You lose`;
+        result = 'computer';
 
     }
-    
+
     else { // Win conditions
-        result = `P: ${playerSelection} <> C: ${computerSelection} -> You win`;
+        result = 'player';
     }
     return result;
 }
 
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    /* game() function = 
+        play 5 rounds of RPS
+        keep score
+        declare winner
+        ask to play again at end of round
+    */
+
+    let wantToPlay = true; //only play if player wants to play; assume true at start
+    let roundNumber = 1;
+    let playerScore = 0;
+    let computerScore = 0;
+
+    while (wantToPlay) {
+        for (roundNumber; roundNumber <= 5; roundNumber++) {
+            const computerSelection = getComputerChoice();
+            const playerSelection = prompt("Please choose: Rock, Paper, Scissors");
+
+            let gameResult = playRound(playerSelection, computerSelection);
+
+            if (gameResult === 'player') { // player wins
+                playerScore += 1;
+            }
+            else if (gameResult === 'computer') { // computer wins
+                computerScore += 1;
+            }
+            else { } // draw
+            console.log(`P: ${playerSelection}\tC: ${computerSelection}`);
+            console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}`);
+        }
+
+        if (playerScore > computerScore) {
+            console.log(`You win! P: ${playerScore} C: ${computerScore}`);
+        }
+        else if (playerScore < computerScore) {
+            console.log(`You lose... P: ${playerScore} C: ${computerScore}`);
+        }
+        else {
+            console.log(`Draw! -- P: ${playerScore} C: ${computerScore}`);
+        }
+        
+        let continuePlaying = prompt("Want to play again? Enter: Y/N.");
+        
+        if (continuePlaying === 'N' ){
+            wantToPlay = false;
+        }
+
+        else { // reset round count and scores to play again
+            roundNumber = 1;
+            playerScore = 0;
+            computerScore = 0;
+        };
+        
+    }
+
+}
+
+
+game();
